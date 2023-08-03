70672b350cbce1df140e42f5925039f95a846cda
Claire Curry <cmcurry@ou.edu>
2023-08-03 18:40:00 -0500

    renamed report files to be not just marketing

--- Create_Report.R
+++ Create_Report.R
@@ -3,9 +3,9 @@
 # any word styles documents must be in same folder as input file directory.
 # output_file is RELATIVE to input file's directory, however: https://github.com/rstudio/rmarkdown/issues/1902
 
-rmarkdown::render(input = "scripts/Research_Workshops_Marketing_Analysis.Rmd", 
+rmarkdown::render(input = "scripts/Research_Workshops_Analysis.Rmd", 
        output_file = paste0("../reports_GO_HERE_FOR_READY_MADE_SLIDES_AND_DATA/",
-                            "Research_Workshops_Marketing_Analysis_",
+                            "Research_Workshops_Analysis_",
                             Sys.Date(),
                             ".docx"),
        output_format = "all")
